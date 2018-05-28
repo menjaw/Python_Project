dab0d1bbe4a18f09cb752aecd4045d7ec9a06e54
menjaw <menjaholm@gmail.com>
2018-05-28 23:09:20 +0200

    modified flask modul in file

--- newPlot.py
+++ newPlot.py
@@ -1,14 +1,15 @@
 import pandas as pd
 from flask import Flask, render_template
 import pygal
-import piePlot 
+import web_view as wv
+#import piePlot
 
 """Series_reference	Period	Data_value	STATUS	UNITS	Subject	Group	Series_title_1"""
 # sep ='/t' is a variable which tell pandas is tabs not commans
-df = pd.read_csv('food-price-index-mar18-weighted-average-prices-csv-tables.csv.tsv',sep='\t')
+df = pd.read_csv('food-price-index-mar18-weighted-average-prices-csv-tables.csv.tsv', sep='\t')
 
 #Initilize program from App
-app = Flask(__name__)
+wv.initialize()
 
 
 @app.route('/plot')
@@ -61,7 +62,7 @@ def show_data():
     i_title = '1 kg beans - Price: {}$'.format(i_res)
     j_title = '1 kg avocado - Price: {}$'.format(j_res)
 
-    ## pygal
+    # pygal
     pie_chart = pygal.Pie()
     pie_chart.title = 'Comparison food price in 2017 in %'
     pie_chart.add(a_title, a_pct)
@@ -75,7 +76,7 @@ def show_data():
     pie_chart.add(i_title, i_pct)
     pie_chart.add(j_title, j_pct)
 
-    #return render_template('pie.html', data = lst_pct, name = title)
+    # return render_template('pie.html', data = lst_pct, name = title)
     return pie_chart.render_response()
 
 
@@ -85,5 +86,6 @@ def calculator(cat, period):
     organges_price = float(organges_df['Data_value'].values)
     return organges_price
 
-app.run(debug=True, port=3000)
+
+wv.run_program()
 

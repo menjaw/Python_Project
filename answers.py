6b96bc7d769529cb06184c5a06a542966afcc85c
Emil Rasmussen <Emil@Studido.com>
2018-05-29 23:08:50 +0200

    Refactored Questions I think a few questions could be spiced up, otherwise i created the file to split up the answers from the rendering to frontend, to provide a clean file structure.

---
+++ answers.py
@@ -0,0 +1,44 @@
+import line_chart_plot as lcp
+
+"""Question 1 - How many products have been tested?"""
+# Should/could be answered with a pie chart
+
+"""Question 2 - How many times have each product been tested?"""
+# Should/could be answered with a bar diagram.
+# Depending on the result from question 1, should consider how many products will be shown.
+
+"""Question 3 - When was 'Tuna - canned' cheapest?"""
+# Should/could be answered with line chart diagram.
+
+"""Question 4 - When was 'Tuna - canned' most expensive?"""
+# Should probably join question 3 and 4 together with.
+
+"""Question 5 - Show the most cheapest product"""
+# Should refrase the question to be more specific in regards to date.
+
+"""Question 6 - Show the most expensive product"""
+# Could be done as a linechart or bardiagram
+
+"""Question 7 - Show the top 10 cheapest food products"""
+# Should/could be answered with a bar diagram.
+
+"""Question 8 - Show the top 10 most expensive food products"""
+# Should/could be answered with a bar diagram.
+
+"""Question 9 - What was the average price for 1 kg bananas in 2012?"""
+# Should/could be answered with line chart diagram.
+
+"""Question 10 - What was the average price for 1 kg bananas in 2012?"""
+
+"""Question 11 - What was the price for 1 kg carrots in marts 2012?"""
+# I think this should maybe be refrased to get more out of the question.
+# Could be a time period such as: from march 2012 - march 2018
+
+"""Question 12 - What was the price for 1 kg carrots in marts 2013?"""
+# Again i think, this could be refrased to provide more data than just a number.
+
+"""In which period was 1 kg kiwi cheapest and what was the price? (Show top 10)"""
+# Line chart
+
+"""In which period was 1 kg kiwi most expensive and what was the price? (Show top 10)"""
+# Just like Kiwi.
